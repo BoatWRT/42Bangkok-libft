165352d3ec178dd2627dd34a89c2c83416541a68
Weerapat Tangchanachaianan <wtangcha@z2t10c1.42bangkok.com>
2023-04-09 15:12:29 +0700

    fix ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: wtangcha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 15:27:02 by wtangcha          #+#    #+#             */
-/*   Updated: 2023/04/09 10:14:49 by wtangcha         ###   ########.fr       */
+/*   Updated: 2023/04/09 15:11:13 by wtangcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,18 @@
 size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
 	size_t	i;
+	size_t	j;
 	size_t	dest_len;
 	size_t	src_len;
 
 	dest_len = ft_strlen(dest);
 	src_len = ft_strlen(src);
+	j = dest_len;
 	i = 0;
-	while (i < size - dest_len - 1 && src[i] != '\0')
+	while (dest_len < size - 1 && size > 0)
 	{
-		dest[dest_len + i] = src [i];
+		dest[j] = src[i];
+	j++;
 	i++;
 	}
 	dest[dest_len + i] = '\0';
