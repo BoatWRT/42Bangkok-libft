abd2d26f424feb9aa65dc36823261a47fdf131f3
Weerapat Tangchanachaianan <wtangcha@z2t10c1.42bangkok.com>
2023-04-09 12:39:03 +0700

    fix libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: wtangcha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 10:36:24 by wtangcha          #+#    #+#             */
-/*   Updated: 2023/04/09 10:37:05 by wtangcha         ###   ########.fr       */
+/*   Updated: 2023/04/09 12:36:03 by wtangcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ void	ft_bzero(void *str, size_t num);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 
-size_t	strlcpy(char *dest, const char *src, size_t size);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 
 int		ft_toupper(int c);
