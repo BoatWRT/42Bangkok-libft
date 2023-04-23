7237b38aad3bf5334d6fae447d0d437ab1bb48cc
Weerapat Tangchanachaianan <wtangcha@z2t10c1.42bangkok.com>
2023-04-23 15:04:43 +0700

    fix malloc 0 and protect ptr

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: wtangcha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 12:26:16 by wtangcha          #+#    #+#             */
-/*   Updated: 2023/04/09 09:56:38 by wtangcha         ###   ########.fr       */
+/*   Updated: 2023/04/23 14:59:15 by wtangcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,11 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
 
-	if (count == 0 || size == 0)
-	{
-	count = 1;
-	size = 1;
-	}
 	if (SIZE_MAX / count < size)
 		return (NULL);
 	ptr = malloc(count * size);
+	if (!ptr)
+		return (NULL);
 	if (ptr)
 		ft_bzero(ptr, count * size);
 	return (ptr);
